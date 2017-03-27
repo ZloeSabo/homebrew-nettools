6c79f4a8e9ff8f60eaa82f6fd62ba83636063e9b
candle <ted@wicrep.com>
2017-03-27 16:25:48 +0900

    update wuzz version to 0.3.0

--- Formula/wuzz.rb
+++ Formula/wuzz.rb
@@ -1,9 +1,9 @@
 class Wuzz < Formula
   desc "Interactive cli tool for HTTP inspection"
   homepage "https://github.com/asciimoo/wuzz"
-  url "https://github.com/asciimoo/wuzz/releases/download/v0.2.0/wuzz_darwin_amd64"
-  version "0.2.0"
-  sha256 "cd2afbca2a6415a618b960000176ba0d76882e93d2feb36588ecb68b3ee32fec"
+  url "https://github.com/asciimoo/wuzz/releases/download/v0.3.0/wuzz_darwin_amd64"
+  version "0.3.0"
+  sha256 "6491a2ec31f83fcaeb1f55e7478fcfb8adde431c22a61d18bdcd208103e133c5"
 
   def install
     mv "wuzz_darwin_amd64", "wuzz"
