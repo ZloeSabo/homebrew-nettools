6b7211789304fb27a611c738269db7f72090f0ee
Evgeny Soynov <saboteur@saboteur.me>
2017-04-20 09:03:03 +0200

    Updated httplab to 0.2.1

--- Formula/httplab.rb
+++ Formula/httplab.rb
@@ -1,8 +1,9 @@
 class Httplab < Formula
   desc "An interactive web server"
   homepage "https://github.com/gchaincl/httplab"
-  url "https://github.com/gchaincl/httplab/releases/download/v0.1.0/httplab_darwin_amd64"
-  sha256 "d0bc5d05e1f749215e46c3d1c7e9fb55ac44e3396546e6fd35bb38d6fc1fa9c4"
+  url "https://github.com/gchaincl/httplab/releases/download/v0.2.1/httplab_darwin_amd64"
+  version "v0.2.1"
+  sha256 "facdb77d54d4fb706b76bd00998c333e742f5ca5152d0e11be4658702f758505"
 
   def install
     mv "httplab_darwin_amd64", "httplab"
@@ -10,6 +11,6 @@ class Httplab < Formula
   end
 
   test do
-    assert_match /httplab: Mach-O 64-bit executable x86_64/, shell_output("file #{bin}/httplab")
+    assert_match /#{version}/, shell_output("#{bin}/httplab -version")
   end
 end
