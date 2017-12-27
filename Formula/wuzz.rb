e90af05a2126ba169df3eaa76bf06e01f248024f
Aaron Bates <aaron@aaronbates.me>
2017-12-27 23:59:22 +0000

    chore: update wuzz to 0.4.0

--- Formula/wuzz.rb
+++ Formula/wuzz.rb
@@ -1,9 +1,9 @@
 class Wuzz < Formula
   desc "Interactive cli tool for HTTP inspection"
   homepage "https://github.com/asciimoo/wuzz"
-  url "https://github.com/asciimoo/wuzz/releases/download/v0.3.0/wuzz_darwin_amd64"
-  version "0.3.0"
-  sha256 "6491a2ec31f83fcaeb1f55e7478fcfb8adde431c22a61d18bdcd208103e133c5"
+  url "https://github.com/asciimoo/wuzz/releases/download/v0.4.0/wuzz_darwin_amd64"
+  version "0.4.0"
+  sha256 "c8a4bc7eef2511093fd55a1e38b90aeabb4b0016b01875ec143ae19358342470"
 
   def install
     mv "wuzz_darwin_amd64", "wuzz"
